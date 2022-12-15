5fd81f4684e8d090f08ea6293f779b25420dbda2
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-12-15 00:09:54 -0500

    Add files via upload

--- leetcode.js
+++ leetcode.js
@@ -130,3 +130,17 @@ var singleNumber = function (nums) {
     }
   }
 };
+
+//141. Linked List Cycle
+var hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+};
